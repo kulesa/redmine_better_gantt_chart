b092ae3fc5e00e8a1530e760b6e9a55d12889427
Alexey Kuleshov <kulesa@gmail.com>
2011-04-04 00:24:02 +0400

    Added test for rescheduling on long issue dependency chain

--- spec/models/issue_patch_spec.rb
+++ spec/models/issue_patch_spec.rb
@@ -21,5 +21,31 @@ describe 'Issue Dependency Patch' do
       @second_issue.reload
     }.should change(@second_issue, :start_date).to(@first_issue.due_date - 1.day)
   end
+
+  describe 'Long dependency chain' do
+    before do
+      @start_issue = Factory(:issue) 
+      @current_issue = @start_issue
+      30.times do 
+        previous_issue, @current_issue = create_related_issues("precedes", @current_issue)
+      end
+    end
+
+    it 'should reschedule tens of related issues when moving due date back' do 
+     lambda {
+        @start_issue.due_date = @start_issue.due_date - 2.days
+        @start_issue.save!
+        @current_issue.reload
+      }.should change(@current_issue, :start_date).to(@current_issue.start_date - 2.days)
+    end
+
+    it 'should reschedule tens of related issues when moving due date forth' do 
+     lambda {
+        @start_issue.due_date = @start_issue.due_date + 2.days
+        @start_issue.save!
+        @current_issue.reload
+      }.should change(@current_issue, :start_date).to(@current_issue.start_date + 2.days)
+    end
+   end
 end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -36,11 +36,9 @@ if defined?(Rcov)
   all_app_files.each{|rb| require rb}
 end
 
-def create_related_issues(relation_type, delay = 0)
-  from_issue, to_issue = Factory(:issue), Factory(:issue)
-  from_issue.relations << IssueRelation.create!(:issue_from => from_issue, :issue_to => to_issue, :relation_type => relation_type, :delay => delay)
+def create_related_issues(relation_type, from_issue = Factory(:issue), to_issue = Factory(:issue))
+  from_issue.relations << IssueRelation.create!(:issue_from => from_issue, :issue_to => to_issue, :relation_type => relation_type)
   from_issue.save!
-
   [from_issue, to_issue].map(&:reload)
 end
 
