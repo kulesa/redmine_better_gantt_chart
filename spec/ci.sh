039b99d6d98f5bc8b01b9dfa537c76f789f6695d
Alexey Kuleshov <kulesa@gmail.com>
2011-10-23 15:31:59 +0400

    Updated CI script

--- spec/ci.sh
+++ spec/ci.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-cd spec && git clone https://github.com/edavis10/redmine.git dummy_redmine && cp ci_config/* dummy_redmine/config/ && cd dummy_redmine && git co 1.1.3 && bundle exec rake db:create db:migrate && cd ../.. && bundle exec rake spec
+cd spec && git clone https://github.com/edavis10/redmine.git dummy_redmine && cp ci_config/database.yml dummy_redmine/config/database.yml && cd dummy_redmine && git co 1.1.3 && bundle exec rake db:create db:migrate && cd ../.. && bundle exec rake spec
