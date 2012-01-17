ca0850ad00b4c18fa7ab41103c4d395a14f7cd76
Alexey Kuleshov <kulesa@gmail.com>
2012-01-17 09:09:05 +0400

    CI script changes: yet another attempt to fix paths in the script

--- spec/ci.sh
+++ spec/ci.sh
@@ -5,8 +5,7 @@ cd spec/dummy_redmine && git checkout origin/1.3-stable
 echo "Copying database.yml"
 cp ../ci_config/database.ci.yml config/database.yml  
 echo "Cloning the plugin to dummy Redmine plugins folder"
-git clone . spec/dummy_redmine/vendor/plugins/redmine_better_gantt_chart
+git clone ../.. vendor/plugins/redmine_better_gantt_chart
 echo "Migrating database"
 RAILS_ENV=test bundle exec rake db:create db:migrate db:migrate_plugins
-cd ../..
-cd spec/dummy_redmine/vendor/plugins/redmine_better_gantt_chart && bundle install
+cd vendor/plugins/redmine_better_gantt_chart && bundle install
