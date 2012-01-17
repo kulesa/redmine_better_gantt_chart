95be8db3c3aaf3be3b54288abc15c28bbc3f8332
Alexey Kuleshov <kulesa@gmail.com>
2012-01-17 08:59:36 +0400

    CI config changed: first add plugin, then run rake db:migrate_plugins

--- spec/ci.sh
+++ spec/ci.sh
@@ -4,9 +4,9 @@ echo "Checking out branch 1.3"
 cd spec/dummy_redmine && git checkout origin/1.3-stable
 echo "Copying database.yml"
 cp ../ci_config/database.ci.yml config/database.yml  
+echo "Cloning the plugin to dummy Redmine plugins folder"
+git clone . spec/dummy_redmine/vendor/plugins/redmine_better_gantt_chart
 echo "Migrating database"
 RAILS_ENV=test bundle exec rake db:create db:migrate db:migrate_plugins
 cd ../..
-echo "Cloning the plugin to dummy Redmine plugins folder"
-git clone . spec/dummy_redmine/vendor/plugins/redmine_better_gantt_chart
 cd spec/dummy_redmine/vendor/plugins/redmine_better_gantt_chart && bundle install
