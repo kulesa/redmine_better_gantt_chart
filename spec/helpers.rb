cc5f2554822bc2f5efa3a78b5b7464c135441c73
Alexey Kuleshov <kulesa@gmail.com>
2012-01-19 02:25:56 +0400

    Minor improvements of spec helpers.

--- spec/helpers.rb
+++ spec/helpers.rb
@@ -8,6 +8,7 @@ module Helpers
   def relate_issues(from_issue, to_issue, relation_type = 'precedes')
    from_issue.relations <<  IssueRelation.create!(:issue_from => from_issue, :issue_to => to_issue, :relation_type => relation_type)
    from_issue.save!
+   [from_issue, to_issue].map(&:reload)
   end
 
   def work_on_weekends(wow)
@@ -15,7 +16,7 @@ module Helpers
   end
   
   # Useful for debugging - draws ASCII gantt chart
-  def draw_tasks(start, tasks)
+  def draw_tasks(start, *tasks)
     tasks.each { |t| t.reload }
     tasks.each { |task| draw_from_date(start, task) }
   end
